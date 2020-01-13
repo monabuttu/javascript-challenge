5f1badce1a5760f41688973ce7a0086bb4b28c9d
monabuttu <53659505+monabuttu@users.noreply.github.com>
2020-01-12 22:38:50 -0500

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,38 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+console.log(tableData);
+
+// get table element
+
+var tableBody = d3.select("tbody");
+
+tableData.forEach((sighting) => {
+    var row = tableBody.append("tr");
+    Object.entries(sighting).forEach(([key,value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    }); 
+});
+
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+
+  var filteredData = tableData.filter(date => 
+    date.datetime == inputValue);
+    
+
+  console.log(filteredData);
+
+});
